1d48664f50bdea2be5d125937ac5e5144bf1f2c4
Li Cheng <li.cheng@rafled.ai>
2025-10-24 10:39:33 +0000

    feat: implement task #1 using Rafled.ai

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TestComponent from './components/TestComponent.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-base-200">
+    <TestComponent />
+  </div>
+</template>
